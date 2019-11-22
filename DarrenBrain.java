188741b59a3f600e87d2015d2f460a06462dfa2e
danadar <56270200+danadar@users.noreply.github.com>
2019-11-22 12:32:40 -0600

    DarrenBrain gains intelligence

--- DarrenBrain.java
+++ DarrenBrain.java
@@ -2,16 +2,106 @@
 public class DarrenBrain implements Brain {
 
 	@Override
-	public Move bestMove(Board board, Piece piece, int limitHeight, Move move) {
-		Move m = new Move();
-		if(Math.random() > .5)
-			m.x = 0;
-		else
-			m.x = board.getWidth()-1;
-		m.y = board.getColumnHeight(m.x);
-		m.piece = piece.nextRotation();
-		m.score = 0;
-		return m;
+	/**
+	 Given a piece and a board, returns a move object that represents
+	 the best play for that piece, or returns null if no play is possible.
+	 See the Brain interface for details.
+	*/
+	public Brain.Move bestMove(Board board, Piece piece, int limitHeight, Brain.Move move) {
+		
+		// Allocate a move object if necessary
+		if (move==null) move = new Brain.Move();
+		
+		double bestScore = 1e20;
+		int bestX = 0;
+		int bestY = 0;
+		Piece bestPiece = null;
+		Piece current = piece;
+		
+		// loop through all the rotations
+		while (true) {
+			final int yBound = limitHeight - current.getHeight()+1;
+			final int xBound = board.getWidth() - current.getWidth()+1;
+			
+			// For current rotation, try all the possible columns
+			for (int x = 0; x<xBound; x++) {
+				int y = board.dropHeight(current, x);
+				if (y<yBound) {	// piece does not stick up too far
+					int result = board.place(current, x, y);
+					if (result <= Board.PLACE_ROW_FILLED) {
+						if (result == Board.PLACE_ROW_FILLED) board.clearRows();
+						
+						double score = rateBoard(board);
+						
+						if (score<bestScore) {
+							bestScore = score;
+							bestX = x;
+							bestY = y;
+							bestPiece = current;
+						}
+					}
+					
+					board.undo();	// back out that play, loop around for the next
+				}
+			}
+			
+			current = current.nextRotation();
+			if (current == piece) break;	// break if back to original rotation
+		}
+
+		if (bestPiece == null) return(null);	// could not find a play at all!
+		else {
+			move.x=bestX;
+			move.y=bestY;
+			move.piece=bestPiece;
+			move.score = bestScore;
+			return(move);
+		}
+	}
+	
+	
+	/*
+	 A simple brain function.
+	 Given a board, produce a number that rates
+	 that board position -- larger numbers for worse boards.
+	 This version just counts the height
+	 and the number of "holes" in the board.
+	 See Tetris-Architecture.html for brain ideas.
+	*/
+	public double rateBoard(Board board) {
+		final int width = board.getWidth();
+		final int maxHeight = board.getMaxHeight();
+		
+		int sumHeight = 0;
+		int holes = 0;
+		
+		// Count the holes, and sum up the heights
+		for (int x=0; x<width; x++) {
+			final int colHeight = board.getColumnHeight(x);
+			sumHeight += colHeight;
+			
+			int y = colHeight - 2;	// addr of first possible hole
+			
+			while (y>=0) {
+				if  (!board.getGrid(x,y)) {
+					holes++;
+				}
+				y--;
+			}
+		}
+		
+		double avgHeight = ((double)sumHeight)/width;
+		
+		// Add up the counts to make an overall score
+		// The weights, 8, 40, etc., are just made up numbers that appear to work
+		if(maxHeight > board.getHeight() / 2)
+		{
+			return (40*maxHeight + 25*avgHeight + 30*holes);
+		}
+		else 
+		{
+			return (10*maxHeight + 20*avgHeight + 25*holes);	
+		}
 	}
 	
 }
